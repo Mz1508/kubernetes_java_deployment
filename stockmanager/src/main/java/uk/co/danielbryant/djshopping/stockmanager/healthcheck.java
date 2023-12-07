8fb10c645525bde8da945f58cd616efa280d0a38
Mz1508 <136124542+Mz1508@users.noreply.github.com>
2023-12-07 18:37:39 -0500

    Update healthcheck.java

--- stockmanager/src/main/java/uk/co/danielbryant/djshopping/stockmanager/healthcheck.java
+++ stockmanager/src/main/java/uk/co/danielbryant/djshopping/stockmanager/healthcheck.java
@@ -1,11 +1,12 @@
  package com.example.springbootmanagementexample;
+ package uk.co.danielbryant.djshopping.stockmanager;
  
  import java.util.Date;
  import org.springframework.web.bind.annotation.GetMapping;
  import org.springframework.web.bind.annotation.RestController;
  
  @RestController
- public class SimpleRestController {
+ public class healthcheck {
      @GetMapping("/health")
      public String health() {
          return "Hello User this is Stockmanager !! " + new Date();
